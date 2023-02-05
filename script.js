35a945835a22b892ab6b0a29edfc3fb20685d1b1
Abdumalik Bakhriddinov <abdumalikcodes7753@gmail.com>
2023-02-05 15:42:52 +0500

    MK-2

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 "use strict";
-const nameOld = 'John';
-let x = 8;
-const isJob = true;
-console.log(x + nameOld);
+// Arrays 
+const colors = ['red', 'green', 'blue'];
+const num = [1, 2, 3, 4, 5];
+// any 
+let all = 1;
+all = 'Hello world!';
+// function
+// void bu functiondan hech qanday ma'lumot qaytarmaydi
+function sayHi(a) {
+    if (typeof a === 'number') {
+        console.log(a.toFixed);
+    }
+    if (typeof a === 'string') {
+        console.log(a.toLocaleLowerCase);
+    }
+}
+const number = [1, 2, 3, 4, 5];
+number.map((item, index) => console.log(item * index));
+const logCar = (car) => {
+    return `Name: ${car.name} , Year: ${car.year === undefined ? 'Not Aviable' : car.year}`;
+};
+console.log(logCar({ name: 'Audi' }));
--- script.ts
+++ script.ts
@@ -1,9 +1,32 @@
-const nameOld: string = 'John';
+// Arrays 
 
-let x: number = 8;
+const colors: string[] = ['red','green','blue']
+const num: number[] = [1,2,3,4,5]
 
-const isJob: boolean = true;
+// any 
 
-const num: null = null
+let all:any = 1
 
-const e: undefined = undefined
+all = 'Hello world!'
+
+// function
+
+// void bu functiondan hech qanday ma'lumot qaytarmaydi
+
+function sayHi(a: number | string): void{
+    if(typeof a === 'number'){
+        console.log(a.toFixed);
+    }
+    if(typeof a === 'string'){
+        console.log(a.toLocaleLowerCase);
+    }
+}
+
+const number: number[] = [1,2,3,4,5]
+
+number.map((item,index) => console.log(item * index));
+
+const logCar = (car:{name: string; year?: number}): string => {
+    return `Name: ${car.name} , Year: ${car.year === undefined ? 'Not Aviable' : car.year}`
+}
+console.log(logCar({name:'Audi'}));
